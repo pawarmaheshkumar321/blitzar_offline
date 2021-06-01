2a9f3a4e69d75b801651f85c195e9aaf9bf88670
Maheshkumar Pawar <pawarmaheshkumar321@gmail.com>
2021-06-01 06:12:12 +0530

    test repo package

---
+++ lib/blitzar_offline.dart
@@ -0,0 +1,11 @@
+library blitzar_offline;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  int calculate_square(int k){
+    return k*k;
+  }
+}
