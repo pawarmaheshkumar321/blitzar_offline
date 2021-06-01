b4b0d958a5a014b3935bc7e34eab34bf6ff489bb
Maheshkumar Pawar <pawarmaheshkumar321@gmail.com>
2021-06-01 21:45:32 +0530

    test repo package

--- lib/blitzar_offline.dart
+++ lib/blitzar_offline.dart
@@ -1,4 +1,6 @@
 library blitzar_offline;
+import 'package:flutter/material.dart';
+import 'extras/style/style.dart';
 
 /// A Calculator.
 class Calculator {
@@ -7,4 +9,60 @@ class Calculator {
 }
 int calculate_square(int k){
   return k*k;
+}
+
+Widget ui_textfield(double textFieldwidth, double textFieldheight, TextEditingController _textf_contro_batmanvalue, String label, String hint){
+  return new Column(
+    children: [
+      new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            width: textFieldwidth,
+            child : new Text("$label",
+              style: TextStyle(
+                fontSize: colorStyle.textfield_text_fontsize,
+                fontWeight: colorStyle.textfield_text_fontweight,
+                color: colorStyle.textfield_label_text_color,
+                fontFamily: '${colorStyle.textfield_text_font_family}',
+              ),),
+          ),
+        ],
+      ),
+
+      new Padding(
+        padding: EdgeInsets.fromLTRB(0, 10, 15, 0),
+        child: new Container(
+          width: textFieldwidth,
+          height: textFieldheight,
+          padding: EdgeInsets.fromLTRB(17, 0, 10, 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(colorStyle.textfield_radius),),
+            border: Border.all(color: colorStyle.textfield_border_color, width: 1.0, style: BorderStyle.solid),
+            //color: colorStyle.textfield_bg_color,
+          ),
+
+          child : new TextField(
+            textAlign: TextAlign.start,
+            controller: _textf_contro_batmanvalue,
+            keyboardType: TextInputType.text,
+            /*inputFormatters: <TextInputFormatter>[
+                                  WhitelistingTextInputFormatter.digitsOnly
+                                ],*/
+            style: TextStyle(color: colorStyle.textfield_text_color, fontSize: colorStyle.textfield_text_fontsize, letterSpacing: 2,fontFamily: '${colorStyle.textfield_text_font_family}'),
+
+            decoration: InputDecoration(
+              //prefixIcon: new Icon(FontAwesomeIcons.amazon,color: Colors.black26,),
+              border: InputBorder.none,
+              hintText: '$hint',
+              hintStyle: new TextStyle(color: colorStyle.textfield_hint_text_color,fontFamily: '${colorStyle.textfield_text_font_family}',fontSize: colorStyle.textfield_hinttext_fontsize),
+              contentPadding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+            ),
+
+
+          ),
+        ),
+      ),
+    ],
+  );
 }
---
+++ lib/extras/style/style.dart
@@ -0,0 +1,400 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class colorStyle{
+
+  //universal
+  static final double universal_radius = 15.0;
+  static final String universal_font_family = 'DMSans';
+  static final universal_title_text_color = Color(0xFF2c2e3b);
+  static final double universal_section_title_font_size = 16.0;
+
+
+  //Appbars second
+  static final double universal_appbar_title_font_size = 20;
+  static final double universal_appbar_subtext_font_size = 17;
+  static final double second_appbar_height = 70;
+  static final String universal_font_family_bottom_navy = 'DMSans';
+
+  //Appbars2
+  static final appbar_backgroundColor = Color(0xFF2c2e3b);
+  static final appbar_title_color = Color(0xFFf8f9fb);
+  static final double appbar_title_font_size = 22.0;
+  static final String appbar_font_family = 'DMSans';
+
+
+
+  //backgrounds and titles
+  static final sc_backgroundColor = Color(0xFFffffff);
+  static final sc_backgroundColor_dark_green = Color(0xFFffffff);
+
+  //static final sc_backgroundColor = Color(0xFFf8f9fb);
+  static final main_bg_color = Color(0xFFffffff);
+  //static final main_bg_color = Color(0xFFe7e8ec);
+  static final title_text_color = Color(0xFF212429);
+  static final double title_text_font_size = 22.0;
+
+  //button
+  static final button_bg_color = Color(0xFFff4236);
+  static final button_text_color = Color(0xFFffffff);
+  static final double button_radius_color = 15.0;
+  static final double button_font_size = 17.0;
+  static final double button_buy_font_size = 17.0;
+
+  //Secondary button
+  static final button_sec_bg_color = Color(0xFFfec107);
+  static final button_sec_text_color = Color(0xFF212429);
+
+
+  //floating action button
+  static final floating_button_bgcolor = Color(0xFF293545);
+  static final floating_button_iconcolor = Color(0xFFffffff);
+
+  //text field
+  static final tf_border_color = Color(0xFFcfd4da);
+  static final tf_text_color = Color(0xFF495058);
+  static final tf_hinttext_color = Color(0xFF6c757e);
+  static final tf_label_color = Color(0xFF202529);
+  static final double tf_radius = 10.0;
+  static final double tf_font_size = 20.0;
+  static final double tf_font_size2 = 17.0;
+  static final double tf_hint_font_size = 20.0;
+
+
+  //snackbar
+  static final snackbar_bg = Color(0xFFff8607);
+  static final snackbar_text_color = Color(0xFF293545);
+  static final double snackbar_font_size = 15;
+
+
+  //floating action button
+  static final floating_bg_color = Color(0xFF00ff00);
+  static final floating_text_color = Color(0xFF293545);
+
+  //faint container
+  static final faintc_bg_color = Color(0xFFFFe8ecef);
+
+  //list item
+  static final li_border_color = Color(0xFFcfd4da);
+  static final li_brand_color = Color(0xFFff4236);
+ // static final li_title_color = Color(0xFFff4236);
+  static final li_modelno_color = Color(0xFF6c757e);
+  static final li_price_color = Color(0xFFff4236);
+
+
+
+
+  //universal
+  static final uni_title_color = Color(0xFF212429);
+  static final uni_dark_title_color = Color(0xFF495058);
+  static final uni_subtext_color = Color(0xFF6c747f);
+  static final uni_link_color = Color(0xFFff4236);
+
+
+  //product
+  static final product_title_color = Color(0xFF212429);
+  static final product_dark_title_color = Color(0xFF495058);
+  static final product_subtext_color = Color(0xFF6c747f);
+  static final product_link_color = Color(0xFFff4236);
+  static final product_q_bg_color = Color(0xFFffffff);
+  //static final product_q_bg_color = Color(0xFFe8ecef);
+  static final double product_p_desc_font_size = 17;
+
+
+
+  //sidebar
+  static final sidebar_backgroundColor = Color(0xFF2c2e3b);
+  static final sidebar_active_title_color = Color(0xFFf8f9fb);
+  static final sidebar_inactive_title_color = Color(0xFFaeaeaf);
+  static final sidebar_divider_color = Color(0xFFaeaeaf);
+  static final double sidebar_title_font_size = 22.0;
+  static final double sidebar_universal_radius = 20;
+  static final String sidebar_font_family = 'DMSans';
+
+
+  //bottom navy bar
+  static const String colorBottomNavyBarInactive = '#AAAFBC';
+  static const String colorBottomNavyBarActive = '#2c2e3b';
+
+
+  //tilltoss
+  static final activity_bodybackground = Color(0xFFf2f2f2);
+
+
+  //tilltoss matches list
+  static final info_button_bodybackground = Color(0xFFe5f2e9);
+  static final info_button_border = Color(0xFFb0e9c0);
+  static final info_button_fontcolor = Color(0xFF4e9d6d);
+  static final pottype_text_fontcolor = Color(0xFF797979);
+  static final matchtype_text_fontcolor = Color(0xFF959595);
+  static final matchshortname_text_color = Color(0xFF000000);
+  static final countdowntimer_text_color = Color(0xFFe00000);
+  static final potprize_text_color = Color(0xFF1a1a1a);
+  static final list_bg_color = Color(0xFFf2f2f2);
+  static final listcard_bg_color = Color(0xFFffffff);
+  static final list_vs_text_color = Color(0xFFe00000);
+
+
+
+  //tilltoss pots list
+  static final entry_button_bodybackground = Color(0xFF00b137);
+  static final entry_button_border = Color(0xFF00b137);
+  static final entry_button_fontcolor = Color(0xFFffffff);
+  static final double entry_button_border_radius = 5;
+
+
+  //tilltoss  pots list info button
+  static final pots_list_info_button_bodybackground = Color(0xFFffffff);
+  static final pots_list_info_button_border = Color(0xFF00b137);
+  static final pots_list_info_button_fontcolor = Color(0xFF959595);
+  static final double pots_list_info_button_border_radius = 30;
+  static final double pots_list_info_button_font_size = 14;
+  static final pots_spotsleft_fontcolor = Color(0xFFf0a436);
+  static final pots_spotstotal_fontcolor = Color(0xFF969696);
+
+  //tilltoss  pots list info button
+  static final pots_tabbar_title_active_color = Color(0xFF000000);
+  static final pots_tabbar_title_inactive_color = Color(0xFF969696);
+  static final pots_tabbar_indicatorColor_color = Color(0xFFf0a436);
+  static final pots_tabbar_background_color = Color(0xFFffffff);
+
+  //tilltoss  pots details
+  static final pots_details_header_color = Color(0xFF969696);
+  static final pots_details_rank_color = Color(0xFF323232);
+  static final pots_details_prize_color = Color(0xFF323232);
+
+
+  //tilltoss  select player screen
+  static final selectedColor_progress_indication_color =  Color(0xFFffbf00);
+  static final notselectedColor_progress_indication_color = Color(0xFF323232);
+  static final player_profile_pic_bg_color = Color(0xFFf0eff4);
+
+  static final player_name_text_color = Color(0xFF323232);
+  static final player_cat_font_color = Color(0xFF969696);
+  static final player_add_icon_color = Color(0xFF00b137);
+  static final next_button_bg_color = Color(0xFF00b137);
+
+  static final selected_player_bg_color = Color(0xFFf3f6ad);
+  static final unselected_player_bg_color = Color(0xFFffffff);
+
+
+
+//tilltoss  team preview screen
+  static final team_player_name_text_color = Color(0xFFffffff);
+  static final team_player_points_text_color = Color(0xFF323232);
+  static final team_user_id_bg_color = Color(0xFF000000);
+  static final team_user_id_text_color = Color(0xFFffffff);
+
+  static final team_user_upper_container_color = Color(0xFF323232);
+  static final team_user_lower_container_color = Color(0xFFffffff);
+
+  //tilltoss  team extra pages
+  static final extra_pages_text_color = Color(0xFF323232);
+
+
+  //floating_action_button
+  static final floating_action_button_bg_color = Color(0xFF2c2e3b);
+
+
+
+
+
+  //tilltoss livescreen team list
+  static final live_score_widget_bg_color = Color(0xFF2c2e3b);
+  static final live_score_match_title_text_color = Color(0xFFffffff);
+  static final live_score_bat_ball_text_color = Color(0xFFaeaeae);
+  static final live_non_striker_text_color = Color(0xFFeedada);
+
+
+  //tilltoss leaderboard user list
+  static final leaderboard_user_id_color = Color(0xFF242424);
+  static final leaderboard_team_id_color = Color(0xFF9d9d9d);
+  static final leaderboard_points_title_color = Color(0xFF878787);
+  static final leaderboard_points_color = Color(0xFF0d0a01);
+  static final leaderboard_rank_title_color = Color(0xFF878787);
+  static final leaderboard_rank_color = Color(0xFF0d0a01);
+
+
+  //tilltoss leaderboard user list
+  static final popup_title_text_color = Color(0xFF323232);
+  static final popup_sub_text_color = Color(0xFF7a7a7a);
+  static final popup_icon_color = Color(0xFF4da2ff);
+  static final popup_button_bg_color = Color(0xFF25ba38);
+  static final popup_button_text_color = Color(0xFFffffff);
+
+
+  //tilltoss match info
+  static final popup_match_info_bg_color = Color(0xFFffffff);
+  static final pinkPrimary = Color(0xFFed556c);
+
+
+
+
+
+
+
+  //token list colors
+  static final token_list_name_text_color = Color(0xFF323232);
+  static final token_list_mobileno_text_color = Color(0xFF323232);
+  static final token_list_crop_text_color = Color(0xFF878787);
+  static final token_list_vehicleno_text_color = Color(0xFF878787);
+  static final token_list_market_name_text_color = Color(0xFFc66f21);
+
+
+  //enter new token
+  static final new_token_MultiSelectChip_title_text_color = Color(0xFF323232);
+  static final new_token_MultiSelectChip_text_color = Color(0xFF878787);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //dashboard
+  static final dashboard_text_color = Color(0xFF001a4e);
+  static final dashboard_subtext_color = Color(0xFF5e6d8a);
+  static final dashboard_list_back_color = Color(0xFFf0f4fd);
+  static final dashboard_icon_back_color = Color(0xFF34308f);
+  static final dashboard_icon_text_color = Color(0xFF244278);
+  static final dashboard_profile_pic_border_color = Color(0xFF34308f);
+  static final dashboard_bottom_button_back_color = Color(0xFF8166e9);
+
+
+
+  //new App theme
+
+
+
+
+  static final new_bg_color = Color(0xFFfdfdff);
+  static final String new_universal_font_family = 'DMSans';
+
+  static final double dashboard_appbar_icon_size = 25;
+
+  static final dashboard_appbar_bg_color = Color(0xFFfdfdff);
+  static final dashboard_appbar_icon_color = Color(0xFF001944);
+  static final double dashboard_appbar_height = 60;
+
+
+  static final dashboard_title_text_color = Color(0xFF001944);
+  static final dashboard_title_subtext_text_color = Color(0xFF606b87);
+  static final String dashboard_title_text_font_family = 'DMSans';
+
+
+  static final double dashboard_title_text_fontsize = 22;
+  static final double dashboard_title_subtexttext_fontsize = 17;
+
+  static final FontWeight new_title_text_fontweight = FontWeight.w700;
+  static final FontWeight newtitle_subtexttext_fontweight = FontWeight.w400;
+
+  static final new_list_bg_color = Color(0xFFf0f4fd);
+  static final new_list_text_color = Color(0xFF00194a);
+  static final new_list_subtext_color = Color(0xFF7f8498);
+  static final new_list_subtext_color_blue = Color(0xFF7c64f0);
+  static final String new_list_font_family = 'DMSans';
+  static final double new_list_text_fontsize = 17;
+  static final double new_list_subtext_fontsize = 14;
+  static final double new_list_radius = 20;
+  static final new_list_icon_color = Color(0xFF34308f);
+
+
+  //textfield
+  static final textfield_label_text_color = Color(0xFF001f46);
+
+
+  static final textfield_bg_color = Color(0xFFf0f4fd);
+  static final textfield_text_color = Color(0xFF516985);
+  static final String textfield_text_font_family = 'DMSans';
+  static final FontWeight textfield_text_fontweight = FontWeight.w600;
+  static final double textfield_text_fontsize = 15;
+
+  static final textfield_hint_text_color = Color(0xFF7c8da0);
+  static final double textfield_hinttext_fontsize = 15;
+
+  static final double textfield_radius = 15;
+
+  static final textfield_border_color = Color(0xFF001944);
+
+
+//button
+  static final new_button_text_color = Color(0xFFfffdff);
+  static final new_button_bg_color = Color(0xFF8166e9);
+  static final double new_button_radius = 15;
+  static final String new_button_text_font_family = 'DMSans';
+  static final double new_button_text_fontsize = 17;
+  static final double new_button_width = 300;
+  static final double new_button_height = 60;
+  static final FontWeight new_button_text_fontweight = FontWeight.w700;
+
+
+  //title
+  static final new_group_title_text_color = Color(0xFF7c64f0);
+  static final FontWeight new_group_title_text_fontweight = FontWeight.w800;
+  static final String new_group_title_text_font_family = 'DMSans';
+  static final double new_group_title_text_fontsize = 15.5;
+
+
+  //admin_app_select_parameters
+  static final select_parameter_checkbox_color = Color(0xFF001944);
+  static final select_parameter_text_color = Color(0xFF5e6d8a);
+  static final double select_parameter_text_fontsize = 14;
+  static final FontWeight select_parameter_text_fontweight = FontWeight.w500;
+
+
+  //transactions color
+
+  static final new_group_key_text_color = Color(0xFF68718e);
+  static final FontWeight new_group_key_text_fontweight = FontWeight.w400;
+  static final String new_group_key_text_font_family = 'DMSans';
+  static final double new_group_key_text_fontsize = 14;
+
+
+  static final new_group_value_text_color = Color(0xFF001740);
+  static final FontWeight new_group_value_text_fontweight = FontWeight.w600;
+  static final String new_group_value_text_font_family = 'DMSans';
+  static final double new_group_value_text_fontsize = 15.5;
+
+  static final transactions_icon_color = Color(0xFF2f2c83);
+
+  static final transactions_divider_color = Color(0xFFc1c9d6);
+
+
+
+
+
+  //bottom nav bar
+  static final bottom_nav_active_icon = Color(0xFF2f2c83);
+  static final bottom_nav_inactive_icon = Color(0xFF6c757e);
+  static final bottom_nav_bg_color = Color(0xFFffffff);
+  //static final bottom_nav_bg_color = Color(0xFFf8f9fb);
+  static final String bottom_nav_font_family = 'DMSans';
+
+
+  //list edit icon button
+  static final list_edit_icon_color = Color(0xFF7c8da0);
+  static final double list_edit_icon_size = 25;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
