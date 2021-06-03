b5fa3d5ea55aaa61dbe2a2bbefd1c17cabacc1c4
Maheshkumar Pawar <pawarmaheshkumar321@gmail.com>
2021-06-03 19:25:34 +0530

    test repo package

--- lib/ui/add_new.dart
+++ lib/ui/add_new.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../extras/style/style.dart';
-
 
 
 
@@ -59,3 +59,175 @@ Widget ui_add_textfield(double textFieldwidth, double textFieldheight, TextEditi
     ],
   );
 }
+
+Widget ui_add_textfield_SingleSelectChip(BuildContext context, double textFieldwidth, double textFieldheight, TextEditingController _textf_contro_batmanvalue, List<String> chips_batmanvalue, String tempresult_batmanvalue){
+  return new Column(
+    children: [
+      new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            width: textFieldwidth,
+            child : new Text("Batmankey",
+              style: TextStyle(
+                fontSize: colorStyle.textfield_text_fontsize,
+                fontWeight: colorStyle.textfield_text_fontweight,
+                color: colorStyle.textfield_label_text_color,
+                fontFamily: '${colorStyle.textfield_text_font_family}',
+              ),),
+          ),
+        ],
+      ),
+      new Padding(
+        padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
+        child: new Container(
+          width: textFieldwidth,
+          height: textFieldheight,
+          padding: EdgeInsets.fromLTRB(17, 0, 10, 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(colorStyle.textfield_radius),),
+            border: Border.all(color: colorStyle.textfield_border_color, width: 1.0, style: BorderStyle.solid),
+            //color: colorStyle.textfield_bg_color,
+          ),
+
+          child : new TextField(
+            textAlign: TextAlign.start,
+            controller: _textf_contro_batmanvalue,
+            keyboardType: TextInputType.text,
+            /*inputFormatters: <TextInputFormatter>[
+                                  WhitelistingTextInputFormatter.digitsOnly
+                                ],*/
+            onTap: (){
+              SystemChannels.textInput.invokeMethod('TextInput.hide');
+              FocusScope.of(context).requestFocus(new FocusNode());
+              _show_batmanvalue_SelectionDialog(context, chips_batmanvalue, "batmanvalue", _textf_contro_batmanvalue, chips_batmanvalue, tempresult_batmanvalue);
+            },
+
+            style: TextStyle(color: colorStyle.textfield_text_color, fontSize: colorStyle.textfield_text_fontsize, letterSpacing: 2,fontFamily: '${colorStyle.textfield_text_font_family}'),
+
+            decoration: InputDecoration(
+              //prefixIcon: new Icon(FontAwesomeIcons.amazon, color: Colors.black26,),
+              border: InputBorder.none,
+              hintText: 'batmanvalue',
+              hintStyle: new TextStyle(color: colorStyle.textfield_hint_text_color,fontFamily: '${colorStyle.textfield_text_font_family}',fontSize: colorStyle.textfield_hinttext_fontsize),
+              contentPadding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+            ),
+
+
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+/////////////////-----------------Single chip function----------------------//
+
+_show_batmanvalue_SelectionDialog(BuildContext context, List<String> chips_list, String dbkey_noun, TextEditingController _textf_contro_batmanvalue, List<String> chips_batmanvalue, String tempresult_batmanvalue) {
+
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        //Here we will build the content of the dialog
+        return new Theme(
+          data: new ThemeData(
+            primaryColor: Colors.blue,
+            primaryColorDark: Colors.black,
+            hintColor: Colors.lightBlueAccent.withOpacity(0.1),
+            backgroundColor: Colors.blue,
+          ),
+
+          child : AlertDialog(
+            title: Text("Select $dbkey_noun", style: new TextStyle(color: Colors.black,
+                fontFamily: colorStyle.universal_font_family,
+                fontWeight: FontWeight.w600,
+                fontSize: 17.5),),
+            backgroundColor: Colors.white,
+            content: SingleSelectChip(
+              chips_list,
+
+              onSelectionChanged: (selectedList) {
+                //setState(() {
+
+                  tempresult_batmanvalue = selectedList[0];
+                  tempresult_batmanvalue = tempresult_batmanvalue.toLowerCase();
+
+                  _textf_contro_batmanvalue.text =  tempresult_batmanvalue;
+
+                //});
+
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: () async{
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          ),
+
+
+        );
+      });
+
+}
+/////////////////-----------------Single chip function----------------------//
+
+
+/////////////////-----------------Single chip class--------------------//
+class SingleSelectChip extends StatefulWidget {
+  final List<String> reportList;
+  final Function(List<String>) onSelectionChanged;
+
+  SingleSelectChip(this.reportList, {this.onSelectionChanged});
+
+  @override
+  _SingleSelectChipState createState() => _SingleSelectChipState();
+}
+
+class _SingleSelectChipState extends State<SingleSelectChip> {
+  // String selectedChoice = "";
+  List<String> selectedChoices = List();
+
+  _buildChoiceList() {
+    List<Widget> choices = List();
+
+    widget.reportList.forEach((item) {
+      choices.add(Container(
+        padding: const EdgeInsets.all(2.0),
+        child: ChoiceChip(
+          label: Text(item),
+          selected: selectedChoices.contains(item),
+          onSelected: (selected) {
+            setState(() {
+
+              selectedChoices.contains(item) ?
+              selectedChoices.clear() : selectedChoices.clear();
+
+              selectedChoices.contains(item)
+                  ? print("present")
+                  : selectedChoices.add(item);
+
+
+              widget.onSelectionChanged(selectedChoices);
+            });
+          },
+        ),
+      ));
+    });
+
+    return choices;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Wrap(
+      children: _buildChoiceList(),
+    );
+  }
+}
+
+/////////////////-----------------Single chip class--------------------//
